552dedee3ce82877c307beb9dbbf08e5be1f727e
tanelmetsar <tanel.metsar@tktk.ee>
2024-04-26 23:08:46 +0300

    add week13 code

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿static SQLiteConnection CreateConnection()
+{
+    SQLiteConnection connection = new SQLiteConnection("Data Source=mydb.db; Version = 3; New = True; Compress = True;");
+
+    try
+    {
+        connection.Open();
+        Console.WriteLine("DB found.");
+    }
+    catch
+    {
+        Console.WriteLine("db not found");
+    }
+
+    return connection;
+}
+
+static void ReadData(SQLiteConnection myConnection)
+{
+    Console.Clear();
+    SQLiteDataReader reader;
+    SQLiteCommand command;
+
+
+    command = myConnection.CreateCommand();
+    command.CommandText = "SELECT * FROM customer";
+    reader = command.ExecuteReader();
+
+    while (reader.Read())
+    {
+        string readerStringFirstName = reader.GetString(0);
+        string readerStringLastName = reader.GetString(1);
+        string readerStringDoB = reader.GetString(2);
+
+        Console.WriteLine($"Full name: {readerStringFirstName}{readerStringLastName}; DoB: {readerStringDoB}");
+    }
+    myConnection.Close();
+}
+
+static void InstertCustomer(SQLiteConnection myConnection)
+{
+    SQLiteCommand command;
+    string fName, lName, dob;
+
+    Console.WriteLine("Enter first name");
+    fName = Console.ReadLine();
+    Console.WriteLine("Enter Last name");
+    lName = Console.ReadLine();
+    Console.WriteLine("Enter date of birth (mm-dd-yyyy):");
+    dob = Console.ReadLine();
+
+    command = myConnection.CreateCommand();
+    command.CommandText = $"INSERT INTO customer(firstName, lastName, dateOfBirth) " +
+    $"VALUES ('{fName}', '{lName}', '{dob}')";
+
+    int rowInserted = command. ();
+    Console.WriteLine($"Row inserted {rowInserted}");
+
+    ReadData(myConnection);
+
+}
+static void RemoveCustomer(SQLiteConnection myConnection)
+{
+    SQLiteCommand command;
+
+    string idToDelete;
+    Console.WriteLine("Enter an id to delet a customer:");
+    idToDelete = Console.ReadLine();
+
+    command = myConnection.CreateCommand();
+    command.CommandText = $"DELETE FROM customer WHERE rowid = {idToDelete}";
+    int rowRemoved = command.ExecuteNonQuery();
+
+}
